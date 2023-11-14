5f5a550c800c51f40fce33c12ed9e7db21899a61
Sebastian Marcelo Suarez <sebastian@Sebastians-MacBook-Pro.local>
2023-11-14 15:05:32 -0300

    cache added

--- src/main/java/com/application/repository/SuperHeroRepository.java
+++ src/main/java/com/application/repository/SuperHeroRepository.java
@@ -1,6 +1,8 @@
 package com.application.repository;
 
 import com.application.model.SuperHero;
+
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -10,8 +12,10 @@ import java.util.Optional;
 public interface SuperHeroRepository extends JpaRepository<SuperHero, Long> {
 
 	@Query("SELECT s FROM SuperHero s")
+	@Cacheable("allSuperHeroes")
 	Optional<List<SuperHero>> findAllSuperHeroes();
 
+	@Cacheable("AllSuperHeroesByName")
 	Optional<List<SuperHero>>findBySuperHeroNameContainingIgnoreCase(String superHeroName);
 
 }
